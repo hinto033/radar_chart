07fd1c0fa145dde82fae17901f359b5dd01eb0df
Ben Hinton <Ben Hinton>
2016-08-31 00:44:49 -0700

    updated R analysis plotting code.

--- Plot-From-EnteredValues/server.R
+++ Plot-From-EnteredValues/server.R
@@ -1,41 +1,29 @@
 # server.R
 
-###Need to:
-##Convert to LMI and FMI
-#finish the final calculations
-#Produce the radar charts.
+##Need to:
+#Make subfnctions and call them
+#Clean up code
+#Clean up variable Names
+#Mess with ordering
+#Fix tabbing
+#Overlay image behind the radar chart
+#Work on the ui.r
+#Think about R Shiny widget types to use.
 
 
+# blackData <- read.table(file=sprintf("data/Black.ZScoreValues.txt", sep="\t"))
+# hispData <- read.table(file=sprintf("data/Hisp.ZScoreValues.txt", sep="\t"))
+# whiteData <- read.table(file=sprintf("data/White.ZScoreValues.txt", sep="\t"))
+# fullData <- rbind(blackData, hispData, whiteData)
+#Maybe have 2 sidebar panels
 
-library(fmsb)
-maxmin <- data.frame(
-  Z_TR=c(2, -2),
-  Z_LA=c(2, -2),
-  Z_LL=c(2, -2),
-  Z_RL=c(2, -2),
-  Z_RA=c(2, -2))
-
-chartDim <- c(1,1)
-
-#setwd('X:\\bhinton\\radar_chart\\Plot-From-DXA')
-
-blackData <- read.table(file=sprintf("data/Black.ZScoreValues.txt", sep="\t"))
-hispData <- read.table(file=sprintf("data/Hisp.ZScoreValues.txt", sep="\t"))
-whiteData <- read.table(file=sprintf("data/White.ZScoreValues.txt", sep="\t"))
-fullData <- rbind(blackData, hispData, whiteData)
-
-#Import the Fit 3D Group
-
-
-
-#  Formula to convert from value (y) to z score (z)
+#  Formula to convert from FMI/LMI value (y) to FMI/LMI z score (z)
 # z = ( y / m)^L - 1 / (L*S)
 
-#Inputs:
-
+#Inputs: The LMS Tables for {White,Black,Hispanic}{Male,Female}{Arm,Leg,Trunk}{FMI,LMI}
 #Specifies which columns to keep from LMS tables
 keep <- c("Age","L", "M", "S")
-
+#Keeps only the relevant columns for the black females
 bfArmFmiLms <- 
   read.table("data/BlackFmiLmi_Female_AvgArmFMI_020202t.txt", header=T, skip=10, sep="\t")
 bfArmLmiLms <- 
@@ -48,11 +36,10 @@ bfTrunkFmiLms <-
   read.table("data/BlackFmiLmi_Female_TrunkFMI_020402t.txt", header=T, skip=10, sep="\t")
 bfTrunkLmiLms <- 
   read.table("data/BlackFmiLmi_Female_TrunkLMI_010401t.txt", header=T, skip=10, sep="\t")
-#Keeps only the relevant columns for the black females
 bfLms <- cbind(bfArmFmiLms[keep], bfArmLmiLms[keep],
                bfLegFmiLms[keep], bfLegLmiLms[keep],
                bfTrunkFmiLms[keep], bfTrunkLmiLms[keep])
-
+#Keeps only the relevant columns for the black males
 bmArmFmiLms <- 
   read.table("data/BlackFmiLmi_Male_AvgArmFMI_020202t.txt", header=T, skip=10, sep="\t")
 bmArmLmiLms <- 
@@ -63,14 +50,12 @@ bmLegLmiLms <-
   read.table("data/BlackFmiLmi_Male_AvgLegLMI_010501t.txt", header=T, skip=10, sep="\t")
 bmTrunkFmiLms <- 
   read.table("data/BlackFmiLmi_Male_TrunkFMI_020401tt.txt", header=T, skip=10, sep="\t")
-#This blew up at 8 yr old and didn't display a number so I put a junk variable in. 
 bmTrunkLmiLms <- 
   read.table("data/BlackFmiLmi_Male_TrunkLMI_010601t.txt", header=T, skip=10, sep="\t")
-#Keeps only the relevant columns for the black males
 bmLms <- cbind(bmArmFmiLms[keep], bmArmLmiLms[keep], 
                bmLegFmiLms[keep], bmLegLmiLms[keep],
                bmTrunkFmiLms[keep], bmTrunkLmiLms[keep])
-
+#Hispanic Females
 hfArmFmiLms <- 
   read.table("data/HispFmiLmi_Female_AvgArmFMI_020302t.txt", header=T, skip=10, sep="\t")
 hfArmLmiLms <- 
@@ -83,12 +68,10 @@ hfTrunkFmiLms <-
   read.table("data/HispFmiLmi_Female_TrunkFMI_020402t.txt", header=T, skip=10, sep="\t")
 hfTrunkLmiLms <- 
   read.table("data/HispFmiLmi_Female_TrunkLMI_020401t.txt", header=T, skip=10, sep="\t")
-#Hispanic Females
 hfLms <- cbind(hfArmFmiLms[keep], hfArmLmiLms[keep], 
                hfLegFmiLms[keep], hfLegLmiLms[keep],
                hfTrunkFmiLms[keep], hfTrunkLmiLms[keep])
-
-
+#Hispanic Males
 hmArmFmiLms <- 
   read.table("data/HispFmiLmi_Male_AvgArmFMI_010403t.txt", header=T, skip=10, sep="\t")
 hmArmLmiLms <- 
@@ -101,11 +84,10 @@ hmTrunkFmiLms <-
   read.table("data/HispFmiLmi_Male_TrunkFMI_020502t.txt", header=T, skip=10, sep="\t")
 hmTrunkLmiLms <- 
   read.table("data/HispFmiLmi_Male_TrunkLMI_010702t.txt", header=T, skip=10, sep="\t")
-#Hispanic Males
 hmLms <- cbind(hmArmFmiLms[keep], hmArmLmiLms[keep], 
                hmLegFmiLms[keep], hmLegLmiLms[keep],
                hmTrunkFmiLms[keep], hmTrunkLmiLms[keep])
-
+#White Females
 wfArmFmiLms <- 
   read.table("data/WhiteFmiLmi_Female_AvgArmFMI_020202t.txt", header=T, skip=10, sep="\t")
 wfArmLmiLms <- 
@@ -118,11 +100,10 @@ wfTrunkFmiLms <-
   read.table("data/WhiteFmiLmi_Female_TrunkFMI_020402t.txt", header=T, skip=10, sep="\t")
 wfTrunkLmiLms <- 
   read.table("data/WhiteFmiLmi_Female_TrunkLMI_010401t.txt", header=T, skip=10, sep="\t")
-#White Females
 wfLms <- cbind(wfArmFmiLms[keep], wfArmLmiLms[keep], 
                wfLegFmiLms[keep], wfLegLmiLms[keep],
                wfTrunkFmiLms[keep], wfTrunkLmiLms[keep])
-
+#White Males
 wmArmFmiLms <- 
   read.table("data/WhiteFmiLmi_Male_AvgArmFMI_020402t.txt", header=T, skip=10, sep="\t")
 wmArmLmiLms <- 
@@ -135,119 +116,92 @@ wmTrunkFmiLms <-
   read.table("data/WhiteFmiLmi_Male_TrunkFMI_020502t.txt", header=T, skip=10, sep="\t")
 wmTrunkLmiLms <- 
   read.table("data/WhiteFmiLmi_Male_TrunkLMI_020702t.txt", header=T, skip=10, sep="\t")
-#White Males
 wmLms <- cbind(wmArmFmiLms[keep], wmArmLmiLms[keep], 
                wmLegFmiLms[keep], wmLegLmiLms[keep],
                wmTrunkFmiLms[keep], wmTrunkLmiLms[keep])
 
+#Set up initial variables for radar chart generation
+library(fmsb)
+maxmin <- data.frame(
+  Z_TR=c(2, -2),
+  Z_RA=c(2, -2),
+  Z_RL=c(2, -2),
+  Z_LL=c(2, -2),
+  Z_LA=c(2, -2))
+chartDim <- c(1,1)
 
-
-    
-
-    #Changes column names to create the radar charts later on
-
-
-
-
+#Beginning of Graphing
 shinyServer(
   function(input, output) {
-
-   
-    
     output$map <- renderPlot({
-
       hfLms <- cbind(hfArmFmiLms[keep], hfArmLmiLms[keep], 
                      hfLegFmiLms[keep], hfLegLmiLms[keep],
                      hfTrunkFmiLms[keep], hfTrunkLmiLms[keep])
-      
-      
       hmLms <- cbind(hmArmFmiLms[keep], hmArmLmiLms[keep], 
                      hmLegFmiLms[keep], hmLegLmiLms[keep],
                      hmTrunkFmiLms[keep], hmTrunkLmiLms[keep])
-      
-      
       bmLms <- cbind(bmArmFmiLms[keep], bmArmLmiLms[keep], 
                      bmLegFmiLms[keep], bmLegLmiLms[keep],
                      bmTrunkFmiLms[keep], bmTrunkLmiLms[keep])
-      
       bfLms <- cbind(bfArmFmiLms[keep], bfArmLmiLms[keep],
                      bfLegFmiLms[keep], bfLegLmiLms[keep],
                      bfTrunkFmiLms[keep], bfTrunkLmiLms[keep])
-      
       wmLms <- cbind(wmArmFmiLms[keep], wmArmLmiLms[keep], 
                      wmLegFmiLms[keep], wmLegLmiLms[keep],
                      wmTrunkFmiLms[keep], wmTrunkLmiLms[keep])
       wfLms <- cbind(wfArmFmiLms[keep], wfArmLmiLms[keep], 
                      wfLegFmiLms[keep], wfLegLmiLms[keep],
                      wfTrunkFmiLms[keep], wfTrunkLmiLms[keep])
-      
-      
-      
-      
       dfit3dBase <- NULL
       
-     
-
-ageYr = input$AgeYr
-height_cm = input$height_cm
-Race = input$Race
-Gender = input$Gender 
-RARM_FAT = input$RARM_FAT
-RARM_LEAN = input$RARM_LEAN
-LARM_FAT = input$LARM_FAT
-LARM_LEAN = input$LARM_LEAN
-L_LEG_FAT = input$L_LEG_FAT
-L_LEG_LEAN = input$L_LEG_LEAN
-R_LEG_FAT = input$R_LEG_FAT
-R_LEG_LEAN = input$R_LEG_LEAN
-TRUNK_FAT = input$TRUNK_FAT
-TRUNK_LEAN = input$TRUNK_LEAN 
-  
-
-
-  trunkFmi = (TRUNK_FAT/1000) / ((height_cm/100)^2)
-  leftArmFmi = (LARM_FAT/1000) / ((height_cm/100)^2)
-  leftLegFmi = (L_LEG_FAT/1000) / ((height_cm/100)^2)
-  rightLegFmi = (R_LEG_FAT/1000) / ((height_cm/100)^2)
-  rightArmFmi = (RARM_FAT/1000) / ((height_cm/100)^2)
-  trunkLmi = (TRUNK_LEAN/1000) / ((height_cm/100)^2)
-  leftArmLmi = (LARM_LEAN/1000) / ((height_cm/100)^2)
-  leftLegLmi = (L_LEG_LEAN/1000) / ((height_cm/100)^2)
-  rightLegLmi = (R_LEG_LEAN/1000) / ((height_cm/100)^2)
-  rightArmLmi = (RARM_LEAN/1000) / ((height_cm/100)^2)
-
-  
+      #Set the relevant variables as your inputs
+      ageYr = input$AgeYr
+      height_cm = input$height_cm
+      Race = input$Race
+      Gender = input$Gender 
+      RARM_FAT = input$RARM_FAT
+      RARM_LEAN = input$RARM_LEAN
+      LARM_FAT = input$LARM_FAT
+      LARM_LEAN = input$LARM_LEAN
+      L_LEG_FAT = input$L_LEG_FAT
+      L_LEG_LEAN = input$L_LEG_LEAN
+      R_LEG_FAT = input$R_LEG_FAT
+      R_LEG_LEAN = input$R_LEG_LEAN
+      TRUNK_FAT = input$TRUNK_FAT
+      TRUNK_LEAN = input$TRUNK_LEAN
+      race = input$Race
+      gender = input$Gender
+      age = input$AgeYr
       
-  race = input$Race
-  gender = input$Gender
-  age = input$AgeYr
+      #Calculate FMI/LMI from that.
+      trunkFmi = (TRUNK_FAT/1000) / ((height_cm/100)^2)
+      leftArmFmi = (LARM_FAT/1000) / ((height_cm/100)^2)
+      leftLegFmi = (L_LEG_FAT/1000) / ((height_cm/100)^2)
+      rightLegFmi = (R_LEG_FAT/1000) / ((height_cm/100)^2)
+      rightArmFmi = (RARM_FAT/1000) / ((height_cm/100)^2)
+      trunkLmi = (TRUNK_LEAN/1000) / ((height_cm/100)^2)
+      leftArmLmi = (LARM_LEAN/1000) / ((height_cm/100)^2)
+      leftLegLmi = (L_LEG_LEAN/1000) / ((height_cm/100)^2)
+      rightLegLmi = (R_LEG_LEAN/1000) / ((height_cm/100)^2)
+      rightArmLmi = (RARM_LEAN/1000) / ((height_cm/100)^2)
       
-
-      
-      
-  if (race == 3){
-    racePrefix = 'b'
-  }else if (race == 2){
-    racePrefix = 'w'
-  }else if (race == 1){
-    racePrefix = 'h'
-  }
-  if (gender == 2){
-    genderPrefix = 'm'
-  }else if (gender == 1){
-    genderPrefix = 'f'
-  }
-      
-      
-  frames <- c(sprintf("%s%sLms", racePrefix, genderPrefix))
-  
-  df <- get(frames)
-  
-  lmsChart <- assign(as.character(frames), df, envir= .GlobalEnv)
-      
-      
-      
-      
+      if (race == 3){
+        racePrefix = 'b'
+      }else if (race == 2){
+        racePrefix = 'w'
+      }else if (race == 1){
+        racePrefix = 'h'
+      }
+      if (gender == 2){
+        genderPrefix = 'm'
+      }else if (gender == 1){
+        genderPrefix = 'f'
+      }
+      #Find the appropriate LMS Table absed on the gender/ethnicity choice
+      frames <- c(sprintf("%s%sLms", racePrefix, genderPrefix))
+      df <- get(frames)
+      lmsChart <- assign(as.character(frames), df, envir= .GlobalEnv)
+      #Select the right age based on the age selection
       agerow = age - 7
       lmsAge <- lmsChart[agerow ,]                           
       #Converts all to data matrix (better for calculations)
@@ -269,124 +223,39 @@ TRUNK_LEAN = input$TRUNK_LEAN
       lTrunkLmi = data.matrix(lmsAge[22])
       mTrunkLmi = data.matrix(lmsAge[23])
       sTrunkLmi = data.matrix(lmsAge[24])
-
+      #Calculate the z scores of every region
+      zLArmFmi= (((leftArmFmi/mArmFmi)^lArmFmi)-1)/(lArmFmi*sArmFmi)
+      zRArmFmi= (((rightArmFmi/mArmFmi)^lArmFmi)-1)/(lArmFmi*sArmFmi)
+      zLArmLmi= (((leftArmLmi/mArmLmi)^lArmLmi)-1)/(lArmLmi*sArmLmi)
+      zRArmLmi= (((rightArmLmi/mArmLmi)^lArmLmi)-1)/(lArmLmi*sArmLmi)
+      zLLegFmi= (((leftLegFmi/mLegFmi)^lLegFmi)-1)/(lLegFmi*sLegFmi)
+      zRLegFmi= (((rightLegFmi/mLegFmi)^lLegFmi)-1)/(lLegFmi*sLegFmi)
+      zLLegLmi= (((leftLegLmi/mLegLmi)^lLegLmi)-1)/(lLegLmi*sLegLmi)
+      zRLegLmi= (((rightLegLmi/mLegLmi)^lLegLmi)-1)/(lLegLmi*sLegLmi)
+      zTrunkFmi= (((trunkFmi/mTrunkFmi)^lTrunkFmi)-1)/(lTrunkFmi*sTrunkFmi)
+      zTrunkLmi= (((trunkLmi/mTrunkLmi)^lTrunkLmi)-1)/(lTrunkLmi*sTrunkLmi)
       
-
-         zLArmFmi= (((leftArmFmi/mArmFmi)^lArmFmi)-1)/(lArmFmi*sArmFmi)
-         zRArmFmi= (((rightArmFmi/mArmFmi)^lArmFmi)-1)/(lArmFmi*sArmFmi)
-         zLArmLmi= (((leftArmLmi/mArmLmi)^lArmLmi)-1)/(lArmLmi*sArmLmi)
-         zRArmLmi= (((rightArmLmi/mArmLmi)^lArmLmi)-1)/(lArmLmi*sArmLmi)
-         zLLegFmi= (((leftLegFmi/mLegFmi)^lLegFmi)-1)/(lLegFmi*sLegFmi)
-         zRLegFmi= (((rightLegFmi/mLegFmi)^lLegFmi)-1)/(lLegFmi*sLegFmi)
-         zLLegLmi= (((leftLegLmi/mLegLmi)^lLegLmi)-1)/(lLegLmi*sLegLmi)
-         zRLegLmi= (((rightLegLmi/mLegLmi)^lLegLmi)-1)/(lLegLmi*sLegLmi)
-         zTrunkFmi= (((trunkFmi/mTrunkFmi)^lTrunkFmi)-1)/(lTrunkFmi*sTrunkFmi)
-         zTrunkLmi= (((trunkLmi/mTrunkLmi)^lTrunkLmi)-1)/(lTrunkLmi*sTrunkLmi)
-      
-      
-
-      
-      
-zScore3 <- data.frame(zLArmFmi,  zRArmFmi,     zLArmLmi, zRArmLmi,  zLLegFmi,  zRLegFmi,
-                       zLLegLmi, zRLegLmi, zTrunkFmi, zTrunkLmi)
-      
-
-      
-      colnames(zScore3) <- c( 'Z_FMI_LA', 'Z_FMI_RA', 
+      #Keep only the necessary columns for radarchart plotting
+      zData2 <- data.frame(zLArmFmi,  zRArmFmi,     zLArmLmi, zRArmLmi,  zLLegFmi,  zRLegFmi,
+                           zLLegLmi, zRLegLmi, zTrunkFmi, zTrunkLmi)
+      colnames(zData2) <- c( 'Z_FMI_LA', 'Z_FMI_RA', 
                               'Z_LMI_LA', 'Z_LMI_RA', 'Z_FMI_LL', 'Z_FMI_RL', 
                               'Z_LMI_LL',  'Z_LMI_RL', 'Z_FMI_TR', 'Z_LMI_TR') 
-      
-###
-      ###
-      ###CURRENTLY NOT CHANGING BETWQEEN GENDERS AND ETHNICITIES
-      ###
-      
-      zData2 <- zScore3 
-      
-      
-      dzData <- data.frame(zData2) 
-      #print(dzData)
-      #print(2)
-      #Converts that set to dataframe
-      #finds dimensions of that table and takes selectNumber random rows from that data set
-      #dimension <- dim(dzData)
-      #nRow <- floor(runif(1, 1,dimension[1]))  #Normally floor(runif(selectNumber, 1,dimension[1]))
-      #selects out only those random rows and their FMI/LMI data
-      fmiData <- dzData[1,c("Z_FMI_TR","Z_FMI_LA", "Z_FMI_LL", "Z_FMI_RL", "Z_FMI_RA")]
-      lmiData <- dzData[1,c("Z_LMI_TR","Z_LMI_LA", "Z_LMI_LL", "Z_LMI_RL", "Z_LMI_RA")]
+      dzData <- data.frame(zData2)
+      #Organizes the FMI/LMI data in format required for radar chart.
+      fmiData <- dzData[1,c("Z_FMI_TR","Z_FMI_RA", "Z_FMI_RL", "Z_FMI_LL", "Z_FMI_LA")]
+      lmiData <- dzData[1,c("Z_LMI_TR","Z_LMI_RA", "Z_LMI_RL", "Z_LMI_LL", "Z_LMI_LA")]
       #renames the columns because column names in fmiData/lmiData must match maxmin
-      colnames(fmiData) <- c("Z_TR", "Z_LA", "Z_LL", "Z_RL", "Z_RA")
-      colnames(lmiData) <- c("Z_TR", "Z_LA", "Z_LL", "Z_RL", "Z_RA")
+      colnames(fmiData) <- c("Z_TR", "Z_RA", "Z_RL", "Z_LL", "Z_LA")
+      colnames(lmiData) <- c("Z_TR", "Z_RA", "Z_RL", "Z_LL", "Z_LA")
       ind1Data <- rbind(maxmin,fmiData[1,],lmiData[1,])   #normally in a loop and i instead of 1
-     
+      #Plots the radar chart data
       op <- par(mar=c(1, 2, 2, 1),mfrow=chartDim)
       radarchart(ind1Data, axistype=3, seg=4, cex.main=1, plty=1, plwd=2, 
                  pcol = c("goldenrod3", "firebrick4"),
                  vlabels=c("TR", "RA", "RL", "LL", "LA"), caxislabels=c("-2","-1","0","1","2"),
                  title="Individual FMI/LMI Chart")
-      legend('topright', c("FMI", "FFMI") , lwd=2, 
+      legend('topright', c("FMI", "LMI") , lwd=2, 
              col=c("goldenrod3", "firebrick4"), bty='n', cex=1.2) 
-      
-      
        })
-    
-#     
-    output$text1 <- renderText({ 
-      #total <- total1[ which(total1$Gender==input$gender 
-       #                      & total1$Race==input$race) , ]
-  
-      Age <- input$AgeYr
-      Gender <- input$Gender
-      Race <- input$Race
-      paste("Age/Gender/Race: ", Age, Gender, Race)
-# 
-# 
-     })
-#     output$text2 <- renderText({ 
-#       #total <- total1[ which(total1$Gender==input$gender 
-#       #                      & total1$Race==input$race) , ]
-#       
-#       zData2 <- FullZSet[input$Person , ] 
-# 
-#       paste("BMI/FMI/LMI: ", zData2$BMI, zData2$FMI, zData2$LMI)
-# 
-#     })
-#     output$text3 <- renderText({ 
-#       #total <- total1[ which(total1$Gender==input$gender 
-#       #                      & total1$Race==input$race) , ]
-#       
-#       zData2 <- FullZSet[input$Person , ] 
-# 
-#       paste("Package ID Number: ", zData2$scan_package_id)
-#     })
-#       
-  }
-    )
-
-
-
-
-
-#      race <- switch(input$race, 
-#                    "Percent White" = counties$white,
-#                   "Percent Black" = counties$black,
-#                  "Percent Hispanic" = counties$hispanic,
-#                 "Percent Asian" = counties$asian)
-
-#      gender <- switch(input$gender, 
-#                     "Percent White" = "darkgreen",
-#                    "Percent Black" = "black",
-#                   "Percent Hispanic" = "darkorange",
-#                  "Percent Asian" = "darkviolet")
-
-#      age <- switch(input$age, 
-#                      "Percent White" = "% White",
-#                     "Percent Black" = "% Black",
-#                    "Percent Hispanic" = "% Hispanic",
-#                   "Percent Asian" = "% Asian")
-
-#percent_map(var = data, 
-#           color = color, 
-#          legend.title = legend,
-#         max = input$range[2], 
-#        min = input$range[1])
+  })
--- Plot-From-EnteredValues/ui.R
+++ Plot-From-EnteredValues/ui.R
@@ -1,75 +1,106 @@
 # ui.R
 
+#http://rmarkdown.rstudio.com/authoring_shiny_widgets.html
+
+
+
 shinyUI(fluidPage(
-  titlePanel("censusVis"),
+
+      tags$head(
+        tags$style(type="text/css", "label.radio { display: inline-block; }", ".radio input[type=\"radio\"] { float: none; }"),
+        tags$style(type="text/css", "select { max-width: 200px; }"),
+        tags$style(type="text/css", "textarea { max-width: 185px;  }"),
+        tags$style(type="text/css", ".jslider { max-width: 200px; }"),
+        tags$style(type='text/css', ".well { max-width: 310px; }"),
+        tags$style(type='text/css', ".span4 { max-width: 310px; }")
+      ),
+  fluidRow(style = "padding-bottom: 5px;",
+          column(2,selectInput("Race", label = h6("Select Race"),
+                               choices = list("Hispanic" = 1, "Non-Hispanic White" = 2, "Non-Hispanic Black" = 3),
+                               selected = 1)),
+          column(2,selectInput("Gender", label = h6("Select Gender"),
+                               choices = list("Female" = 1, "Male" = 2), selected = 1)),
+          column(2,numericInput("AgeYr", label = h6("Enter Age"),value=20)),
+          column(2,numericInput("height_cm", label = h6("Enter height in cm"),value=180)),
+          column(2,numericInput("TRUNK_FAT", label = h6("Enter Trunk Fat Mass in Grams"),value = 10000)),
+          column(2,numericInput("TRUNK_LEAN", label = h6("Enter Trunk Lean Mass in Grams"),value = 25000)) ),
+  fluidRow(style = "padding-bottom: 5px;",
+          column(2,numericInput("RARM_FAT", label = h6("Enter Right Arm Fat Mass in Grams"),value = 1500)),
+          column(2,numericInput("RARM_LEAN", label = h6("Enter Right Arm Lean Mass in Grams"),value = 4000)),
+          column(2,numericInput("LARM_FAT", label = h6("Enter Left Arm Fat Mass in Grams"),value = 1500)),
+          column(2,numericInput("LARM_LEAN", label = h6("Enter Left Arm Lean Mass in Grams"),value = 4000)),
+          column(2,numericInput("R_LEG_FAT", label = h6("Enter Right leg Fat Mass in Grams"),value = 4000)),
+          column(2,numericInput("R_LEG_LEAN", label = h6("Enter Right leg Lean Mass in Grams"),value = 10000)),
+          column(2,numericInput("L_LEG_FAT", label = h6("Enter Left leg Fat Mass in Grams"),value = 4000)),
+          column(2,numericInput("L_LEG_LEAN", label = h6("Enter Left leg Lean Mass in Grams"),value = 10000)) ),
+
+
   
-  sidebarLayout(
-    sidebarPanel(
-      helpText("Create radar charts of  individuals from Fit3d Scans"),
-      
+  fluidRow( plotOutput("map"))
+))
+  # 
+  # titlePanel("Radar Chart Generator"),
+  # 
+  # sidebarLayout(
+  #   sidebarPanel(
 
-          numericInput("AgeYr", 
-                    label = h3("Enter Age"), 
-                    value = 20) , 
-          
-          numericInput("height_cm", 
-                   label = h3("Enter Height in cm"), 
-                   value = 180)   ,
-          
-          
-          
-          selectInput("Race", label = h3("Select Race"), 
-                 choices = list("Hispanic" = 1, "Non-Hispanic White" = 2,
-                                "Non-Hispanic Black" = 3), selected = 1),
-          
-          
-          selectInput("Gender", label = h3("Gender"), 
-                 choices = list("Female" = 1, "Male" = 2), selected = 1),
-          
-          
-          numericInput("RARM_FAT", 
-                   label = h3("Enter Right Arm Fat Mass in Grams"), 
-                   value = 1500),   
-          numericInput("RARM_LEAN", 
-                   label = h3("Enter Right Arm Lean Mass in Grams"), 
-                   value = 4000),   
-          numericInput("LARM_FAT", 
-                 label = h3("Enter Left Arm Fat Mass in Grams"), 
-                 value = 1500),   
-          numericInput("LARM_LEAN", 
-                 label = h3("Enter Left Arm Lean Mass in Grams"), 
-                 value = 4000),   
-          numericInput("L_LEG_FAT", 
-               label = h3("Enter Left Leg Fat Mass in Grams"), 
-               value = 4000),   
-          numericInput("L_LEG_LEAN", 
-               label = h3("Enter Left Leg Lean Mass in Grams"), 
-               value = 10000),  
-          numericInput("R_LEG_FAT", 
-               label = h3("Enter Right Leg Fat Mass in Grams"), 
-               value = 4000),   
-          numericInput("R_LEG_LEAN", 
-               label = h3("Enter Right Leg Lean Mass in Grams"), 
-               value = 10000),   
-          numericInput("TRUNK_FAT", 
-               label = h3("Enter Trunk Fat Mass in Grams"), 
-               value = 10000),   
-          numericInput("TRUNK_LEAN", 
-               label = h3("Enter Trunk Lean Mass in Grams"), 
-               value = 25000)   
-          
-      ),#Sidebar panel
-            
-      #Later: options for a certain age range, or options for certian z scores
-     # ),
-    
-          mainPanel(plotOutput("map"),
-                    br(),
-                    textOutput("text1"),
-                    br()
-
-    )
-    )#Sidebar Layout
+  #     helpText("Create radar charts of  individuals from DXA Data"),
+  #         numericInput("AgeYr", 
+  #                label = h6("Enter Age"), 
+  #                value = 20) , 
+  #     tags$style(type="text/css", "input.shiny-bound-input { font-size:12px; height:20px;}"),
+  #     tags$style(type="text/css", "select.shiny-bound-input { font-size:12px; height:20px;}"),
+  #         numericInput("height_cm", 
+  #                label = h6("Enter Height in cm"), 
+  #                value = 180)   ,
+  #         selectInput("Race", label = h6("Select Race"), 
+  #                choices = list("Hispanic" = 1, "Non-Hispanic White" = 2,
+  #                               "Non-Hispanic Black" = 3), selected = 1),
+  #         selectInput("Gender", label = h6("Gender"), 
+  #                choices = list("Female" = 1, "Male" = 2), selected = 1),
+  #         numericInput("RARM_FAT", 
+  #                label = h6("Enter Right Arm Fat Mass in Grams"), 
+  #                value = 1500),   
+  #         numericInput("RARM_LEAN", 
+  #                label = h6("Enter Right Arm Lean Mass in Grams"), 
+  #                value = 4000),   
+  #         numericInput("LARM_FAT", 
+  #                label = h6("Enter Left Arm Fat Mass in Grams"), 
+  #                value = 1500),   
+  #         numericInput("LARM_LEAN", 
+  #                label = h6("Enter Left Arm Lean Mass in Grams"), 
+  #                value = 4000),   
+  #         numericInput("L_LEG_FAT", 
+  #                label = h6("Enter Left Leg Fat Mass in Grams"), 
+  #                value = 4000),   
+  #         numericInput("L_LEG_LEAN", 
+  #                label = h6("Enter Left Leg Lean Mass in Grams"), 
+  #                value = 10000),  
+  #         numericInput("R_LEG_FAT", 
+  #                label = h6("Enter Right Leg Fat Mass in Grams"), 
+  #                value = 4000),   
+  #         numericInput("R_LEG_LEAN", 
+  #                label = h6("Enter Right Leg Lean Mass in Grams"), 
+  #                value = 10000),   
+  #         numericInput("TRUNK_FAT", 
+  #                label = h6("Enter Trunk Fat Mass in Grams"), 
+  #                value = 10000),   
+  #         numericInput("TRUNK_LEAN", 
+  #                label = h6("Enter Trunk Lean Mass in Grams"), 
+  #                value = 25000)   
+  #         
+    #   ),#Sidebar panel
+    #         
+    #   #Later: options for a certain age range, or options for certian z scores
+    #  # ),
+    # 
+    #       mainPanel(plotOutput("map"),
+    #                 br(),
+    #                 textOutput("text1"),
+    #                 br()
+    # 
+    # )
+    # )#Sidebar Layout
 
 
-))#Shiny ui and fluid page
+# ))#Shiny ui and fluid page
